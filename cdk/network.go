95c7ba3a119e8b0132ec28801d23720cd5ea2406
Frenek Lopez <eric.frenek.lr@gmail.com>
2026-04-12 14:10:08 -0700

    Fix: Force public subnets and correct MaxAzs data type

--- cdk/network.go
+++ cdk/network.go
@@ -14,8 +14,14 @@ func NewVPC(stack awscdk.Stack, id string, props *UmamiStackProps) awsec2.Vpc {
 
 	return awsec2.NewVpc(stack, jsii.String("VPC"), &awsec2.VpcProps{
 		VpcName:     jsii.String(vpcName),
-		MaxAzs:      jsii.Number(props.Config.VPC.MaxAZs),
+		MaxAzs:      jsii.Number(2),
 		NatGateways: jsii.Number(0),
+		SubnetConfiguration: &[]*awsec2.SubnetConfiguration{
+			{
+				Name:       jsii.String("PublicSubnet"),
+				SubnetType: awsec2.SubnetType_PUBLIC,
+			},
+		},
 	})
 
 }
